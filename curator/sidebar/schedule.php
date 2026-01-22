a306ded664ac6d1ca2e1c49f86ec4ec64fa9e65f
ajian_nedo <asianerd28@gmail.com>
2026-01-22 16:29:50 +0800

    Frontend mockup of tasks sidebar

--- curator/sidebar/schedule.php
+++ curator/sidebar/schedule.php
@@ -1,6 +1,60 @@
 <div id="schedule">
-    <div id="query">
-    </div>
-    <div id="overview">
+    <div id="container">
+        <div>
+            <h5>Day</h5>
+            <div>
+                <h4>Max</h4>
+                <img src="../../assets/leaf.svg">
+            </div>
+        </div>
+        <div>
+            <h5>Monday</h5>
+            <div>
+                <h4>500</h4>
+                <img src="../../assets/leaf.svg">
+            </div>
+        </div>
+        <div>
+            <h5>Tuesday</h5>
+            <div>
+                <h4>500</h4>
+                <img src="../../assets/leaf.svg">
+            </div>
+        </div>
+        <div>
+            <h5>Wednesday</h5>
+            <div>
+                <h4>500</h4>
+                <img src="../../assets/leaf.svg">
+            </div>
+        </div>
+        <div>
+            <h5>Thursday</h5>
+            <div>
+                <h4>500</h4>
+                <img src="../../assets/leaf.svg">
+            </div>
+        </div>
+        <div>
+            <h5>Friday</h5>
+            <div>
+                <h4>500</h4>
+                <img src="../../assets/leaf.svg">
+            </div>
+        </div>
+        <div>
+            <h5>Saturday</h5>
+            <div>
+                <h4>500</h4>
+                <img src="../../assets/leaf.svg">
+            </div>
+        </div>
+        <div>
+            <h5>Sunday</h5>
+            <div>
+                <h4>500</h4>
+                <img src="../../assets/leaf.svg">
+            </div>
+        </div>
     </div>
 </div>
